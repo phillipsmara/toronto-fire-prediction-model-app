94d972fe7c7f619f247901b7e3df2b1bdc93cbc6
Mara Phillips <m24phill@uwaterloo.ca>
2026-02-21 15:04:16 -0500

    created outline for data preprocessing

---
+++ src/data/preprocessing.py
@@ -0,0 +1,22 @@
+# Preprocesses data
+import pandas as pd
+
+def load_raw_data(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
+def clean_data_run_areas(df: pd.DataFrame):
+    
+    return df
+
+def clean_data_stations(df: pd.DataFrame):
+
+    return df
+
+def clean_data_hydrants(df: pd.DataFrame):
+
+    return df
+
+def clean_data_incidents(df: pd.DataFrame):
+
+    return df
